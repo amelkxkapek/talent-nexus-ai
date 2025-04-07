756cef9e73535a76239a68fabddf76de65329375
amelkxkapek <5-organy-udziec@icloud.com>
2025-04-07 10:36:38 +0200

    Update supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://isgyxpefhwdcibdxyvuk.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlzZ3l4cGVmaHdkY2liZHh5dnVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM5NTkyMDIsImV4cCI6MjA1OTUzNTIwMn0.u2srWLw6Nf-C4ePR8M3SCNdHJZ42QLv66QX_OcbdEjc';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
